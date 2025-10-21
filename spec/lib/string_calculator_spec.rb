4c3995aa90fb1234e3df5d50391970cb8054f9bd
gyanesh <gyanesh2248@gmail.com>
2025-10-22 03:07:55 +0530

    testcase to support delimiter

--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -16,4 +16,8 @@ RSpec.describe StringCalculator do
   it 'returns sum even if new line is present between numbers' do
     expect(StringCalculator.add("1\n2,5")).to eq(8)
   end
+
+  it 'should supports delimiter "//;\\n1;2"' do
+    expect(StringCalculator.add("//;\n1;2")).to eq(3)
+  end
 end
