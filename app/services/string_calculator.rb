e3c8f41aff4232ae490b62afde25c6f0ed406a65
gyanesh <gyanesh2248@gmail.com>
2025-10-22 03:08:58 +0530

    logic to handle delimiter

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,7 +1,12 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    nums = numbers.split(/[\n,]/).map(&:to_i)
+     delimiter, number = if numbers.start_with?("//")
+      [Regexp.escape(numbers[2]), numbers.split("\n", 2).last]
+    else
+      [/[\n,]/, numbers]
+    end
+     nums = number.split(/#{delimiter}/).map(&:to_i)
     nums.sum
   end
 end 
